1514f62af769ef6862d1bf85d4adc7d191751d95
Cody Flatla <codyflatla@outlook.com>
2020-12-30 23:17:21 -0700

    Apply latest PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -495,8 +495,6 @@
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
-  // ANET A8 Standard Extruder at 210 Degree Celsius and 100% Fan
-  // (measured after M106 S255 with M303 E0 S210 C8)
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify between 1 and HOTENDS values per array.
     // If fewer than EXTRUDER values are provided, the last element will be repeated.
@@ -504,9 +502,9 @@
     #define DEFAULT_Ki_LIST {   1.25,   1.25 }
     #define DEFAULT_Kd_LIST {  86.00,  86.00 }
   #else
-    #define DEFAULT_Kp  21.00
-    #define DEFAULT_Ki   1.25
-    #define DEFAULT_Kd  86.00
+    #define DEFAULT_Kp  14.44
+    #define DEFAULT_Ki   0.65
+    #define DEFAULT_Kd  80.15
   #endif
 #endif // PIDTEMP
 
