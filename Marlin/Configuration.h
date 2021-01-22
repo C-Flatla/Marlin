e4c5c4eca10542103d07a0e68692561cc9bfcb85
Cody Flatla <codyflatla@outlook.com>
2021-01-21 22:16:38 -0700

    Update axis steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 114 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 99.9, 98.62, 400, 114 }
 
 /**
  * Default Max Feed Rate (mm/s)
