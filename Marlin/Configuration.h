b7028dd4bb108cc0e0a3e9e999d0725f71e38678
Cody Flatla <codyflatla@outlook.com>
2023-09-01 03:34:53 -0600

    Adjust probe offset for linear rail upgrade

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1483,11 +1483,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -10, 62, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -5, -45, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
@@ -1698,7 +1698,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -15
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
