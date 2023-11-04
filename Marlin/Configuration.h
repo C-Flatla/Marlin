2998ccc60d640ecf8d0241375c81c69e4416739f
Cody Flatla <codyflatla@outlook.com>
2023-11-04 13:47:03 -0600

    Disable BLTouch, change PETG bed preheat temp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -28,7 +28,7 @@
  */
 
 // Is the BLTouch option installed?
-#define CR10V2_BLTOUCH
+// #define CR10V2_BLTOUCH
 
 /**
  * Configuration.h
@@ -2254,7 +2254,7 @@
 
 #define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 230
-#define PREHEAT_2_TEMP_BED    80
+#define PREHEAT_2_TEMP_BED    70
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
